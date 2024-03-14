00b8e7c0dacfb8323c05b3ee4ba947a16c245780
MelinaClouet <melina.clouet@my-digital-school.org>
2024-03-14 11:48:25 +0100

    [UPD] page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,91 +7,94 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
 </head>
 <body class="" style="background-color: #FAE8E0 ">
-    @include ('layouts.header')
-    <div class="flex justify-between mt-16">
-        <div class="relative flex static w-1/3">
-            <div class="absolute z-0 mt-10 -ml-24 h-52 w-52 transform rotate-45">
-                <img src="/assets/1.png" alt="My image" class="h-full w-full object-cover rounded-full">
-            </div>
-            <div class="absolute z-0 h-52 w-52 transform rotate-90 mt-5">
-                <img src="/assets/8.png" alt="My image" class="h-full w-full object-cover rounded-full">
-            </div>
-
+@include ('layouts.headerHome')
+<div class="flex justify-between mt-28">
+    <div class="relative flex static w-1/3">
+        <div class="absolute z-0 mt-10 -ml-24 h-52 w-52 transform rotate-45">
+            <img src="/assets/1.png" alt="My image" class="h-full w-full object-cover rounded-full">
+        </div>
+        <div class="absolute z-0 h-52 w-52 transform rotate-90 mt-5">
+            <img src="/assets/8.png" alt="My image" class="h-full w-full object-cover rounded-full">
         </div>
 
-        <div class="w-1/3">
-            <div class="rounded-full overflow-hidden h-80 w-72  ">
-                <img src="/assets/enfantDos.jpg" alt="My image" class=" object-cover rounded-full">
-            </div>
-        </div>
+    </div>
 
-
-
-        <div class="w-1/3 ">
-            <div class="flex items-center justify-center mr-24">
-                <p class="montserrat text-2xl mt-10 text-center">
-                    MAYUDA, <br> qu'est ce que c'est ?
-                </p>
-            </div>
-            <div class="mt-7 mr-24 montserrat">
-                <p class="text-justify">
-                    Une entreprise spécialisée dans le bien-être de l'enfant et de l'adolescent. Un accompagnement personnalisé et adapté aux besoins de chacun. Des formations de qualité pour enfants, adolescents et adultes.
-                </p>
-            </div>
-
-            <div class="rounded-full overflow-hidden h-32 w-32 mt-12 ">
-                <img src="/assets/7.png" alt="My image" class="h-full w-full object-cover rounded-full">
-            </div>
-
-            <div class="rounded-full overflow-hidden h-32 w-32 -mt-24 ml-28">
-                <img src="/assets/8.png" alt="My image" class="h-full w-full object-cover rounded-full">
-            </div>
+    <div class="w-1/3">
+        <div class="rounded-full overflow-hidden h-80 w-72  ">
+            <img src="/assets/enfantDos.jpg" alt="My image" class=" object-cover rounded-full">
         </div>
     </div>
 
-    <div class="flex justify-between">
-        <div class="bg-purple-600 w-full flex justify-between items-center" style="background-color: #F8B59C;">
-            <p class="ml-4">Text</p>
-            <div class="-mr-20">
-                <img src="/assets/7.png" alt="My image" class="object-cover rounded-full h-36">
-            </div>
+
+    <div class="w-1/3 ">
+        <div class="flex items-center justify-center mr-24">
+            <p class="montserrat text-2xl mt-10 text-center">
+                MAYUDA, <br> qu'est ce que c'est ?
+            </p>
+        </div>
+        <div class="mt-7 mr-24 montserrat">
+            <p class="text-justify">
+                Une entreprise spécialisée dans le bien-être de l'enfant et de l'adolescent. Un accompagnement
+                personnalisé et adapté aux besoins de chacun. Des formations de qualité pour enfants, adolescents et
+                adultes.
+            </p>
         </div>
 
-        <div class="w-full flex justify-center items-center">
-           <p>Image</p>
+        <div class="rounded-full overflow-hidden h-32 w-32 mt-12 ml-28 ">
+            <img src="/assets/7.png" alt="My image" class="h-full w-full object-cover rounded-full">
+        </div>
+
+        <div class="rounded-full overflow-hidden h-36 w-36 -mt-24 ml-52">
+            <img src="/assets/8.png" alt="My image" class="h-full w-full object-cover rounded-full">
+        </div>
+    </div>
+</div>
+
+<div class="flex justify-between">
+    <div class="bg-purple-600 w-full flex justify-between items-center" style="background-color: #F8B59C;">
+        <p class="ml-4">Text</p>
+        <div class="-mr-20">
+            <img src="/assets/7.png" alt="My image" class="object-cover rounded-full h-36">
         </div>
     </div>
 
-    <div style="background-color: #FD6D2F;">
-        <div class="flex items-center justify-center ">
-            <p class="text-white montserrat text-lg mt-5 font-bold">NOS SERVICES</p>
+    <div class="w-full flex justify-center items-center">
+        <p>Image</p>
+    </div>
+</div>
+
+<div style="background-color: #FD6D2F;">
+    <div class="flex items-center justify-center ">
+        <p class="text-white montserrat text-lg mt-5 font-bold">NOS SERVICES</p>
+    </div>
+    <div class="-mt-24 flex justify-end -mr-3">
+        <img src="/assets/11.png" alt="My image" class="object-cover rounded-full h-36">
+    </div>
+    <div class="flex justify-evenly">
+        <div>
+            <img src="/assets/5.png" alt="Image 1" class="object-cover h-36">
+            <p class="text-center text-white montserrat font-semibold mt-4">
+                Soutient <br> scolaire
+            </p>
         </div>
-        <div class="-mt-24 flex justify-end">
-            <img src="/assets/6.png" alt="My image" class="object-cover rounded-full h-36">
+        <div>
+            <img src="/assets/4.png" alt="Image 2" class="object-cover h-36">
+            <p class="text-center text-white montserrat font-semibold mt-4">Développement <br> personnel</p>
         </div>
-        <div class="flex justify-evenly">
-            <div>
-                <img src="/assets/5.png" alt="Image 1" class="object-cover h-36" >
-                <p class="text-center text-white montserrat font-semibold mt-4">
-                    Soutient <br> scolaire
-                </p>
-            </div>
-            <div>
-                <img src="/assets/4.png" alt="Image 2" class="object-cover h-36" >
-                <p class="text-center text-white montserrat font-semibold mt-4">Développement <br> personnel</p>
-            </div>
-            <div>
-                <img src="/assets/3.png" alt="Image 3" class="object-cover h-36" >
-                <p class="text-center text-white montserrat font-semibold mt-4">Conseil <br> parental</p>
-            </div>
+        <div>
+            <img src="/assets/3.png" alt="Image 3" class="object-cover h-36">
+            <p class="text-center text-white montserrat font-semibold mt-4">Conseil <br> parental</p>
         </div>
     </div>
-    @include ('layouts.contact')
+</div>
+@include ('layouts.contact')
 
-    @include ('layouts.footer')
+@include ('layouts.footer')
 </body>
 </html>
--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -3,21 +3,26 @@
 <div class="w-full flex h-1/3" style="background-color: #9C84C2">
     <div class="w-1/2  mb-12  mt-12">
         <p class="madimi-one-regular text-3xl flex justify-center" style="color: #FAE8E0">Nous contacter!</p>
-        <div class="flex justify-evenly mt-28" style="color: #FAE8E0">
-            <p class="montserrat font-semibold text-xl"> MAIL</p>
-            <p class="montserrat ">mayuda@gmail.com</p>
-        </div>
-        <div class="flex justify-evenly mt-12" style="color: #FAE8E0">
-            <p class="montserrat font-semibold text-xl">RÉSEAUX SOCIAUX</p>
-            <p class="montserrat ">@mayuda</p>
-        </div>
-        <div class="flex justify-evenly mt-12" style="color: #FAE8E0">
-            <p class="montserrat font-semibold text-xl">ADRESSE</p>
-            <p class="montserrat ">20 rue ........</p>
-        </div>
-    </div>
 
-    <div class="w-1/2 bg-green-500">
+        <img src="/assets/6.png" alt="Image 2" class="absolute h-1/5 right-1/2 -mr-16 mt-10">
+
+        <div class="w-2/3 mx-auto">
+            <div class="flex justify-between mt-20" style="color: #FAE8E0">
+                <p class="montserrat font-semibold text-xl">MAIL</p>
+                <p class="montserrat">mayuda@gmail.com</p>
+            </div>
+            <div class="flex justify-between mt-12" style="color: #FAE8E0">
+                <p class="montserrat font-semibold text-xl">RÉSEAUX SOCIAUX</p>
+                <p class="montserrat">@mayuda</p>
+            </div>
+            <div class="flex justify-between mt-12" style="color: #FAE8E0">
+                <p class="montserrat font-semibold text-xl">ADRESSE</p>
+                <p class="montserrat">20 rue ........</p>
+            </div>
+        </div>
+
+    </div>
+    <div class="w-1/2">
         <img src="/assets/contact2.jpg" alt="Image 2" class=" h-full w-full" >
     </div>
 </div>
