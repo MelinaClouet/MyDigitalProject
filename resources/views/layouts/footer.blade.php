2794b260c66f05b6efae23a8134634414d535374
MelinaClouet <melina.clouet@my-digital-school.org>
2024-05-26 12:38:02 +0200

    [ADD] mentions legales

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="bg-beige m-2">
     <footer class="bg-beige">
         <div class="flex justify-center">
-            <a href="{{ route('legalNotice') }}" class="text-orange font-bold text-lg p-4 ">Mentions légales</a>
+            <a href="{{ route('legalNotice') }}" class=" font-poetsen text-orange font-bold text-lg p-4 ">Mentions légales</a>
         </div>
     </footer>
 </div>
